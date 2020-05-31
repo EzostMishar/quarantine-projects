4de5e0d36a4e259e34149d915144c3517dcf650c
Cass Chellis <cche0071@student.monash.edu>
2020-05-31 19:02:14 +1000

    Failed attempt at printing trombonetype to file

--- TromboneType/keyReader.py
+++ TromboneType/keyReader.py
@@ -4,11 +4,19 @@ ser.port = 'COM4'
 ser.open()
 ser.readline()
 
+output_file = open("output.txt","w")
+
+
 while True:
     try:
         ser_bytes = ser.readline().strip().decode("ascii")
 
         print(ser_bytes) 
+
+        if ser_bytes[0] == 1:
+            break
     except:
         print("Keyboard Interrupt")
         break
+
+output_file.write(ser_bytes[2])
